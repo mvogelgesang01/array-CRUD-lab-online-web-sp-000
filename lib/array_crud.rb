137b4ba71928d695f073bb36aa26ee7512d490fc
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-25 20:14:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-["lil' bub", "grumpy cat", "Maru", "big bub"]
+["house", "car", "boat", "fish"]
 end
 
 def add_element_to_end_of_array(array, element)
